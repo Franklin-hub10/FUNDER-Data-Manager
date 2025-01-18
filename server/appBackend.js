1a8f1ddf3232e327b128bbc810c8cf3fa6867ef9
Franklin-hub10 <fffdesmond@gmail.com>
2025-01-18 12:20:53 -0500

    ff

--- server/appBackend.js
+++ server/appBackend.js
@@ -1,24 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-
-// Importar rutas
-const authRoutes = require('./routes/auth');
-const roleRoutes = require('./routes/roles');
-const userRoutes = require('./routes/users');
+const rolesRoutes = require('./routes/roles');
 
 const app = express();
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Rutas
-app.use('/api/auth', authRoutes);
-app.use('/api/roles', roleRoutes);
-app.use('/api/users', userRoutes);
+app.use('/roles', rolesRoutes);
 
-// Puerto del servidor
+// Inicio del servidor
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
